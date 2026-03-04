0155ce6476449a98d0ecb7e07060a4e430dfbee5
deanbaba451-del <deanbaba451@gmail.com>
2026-03-04 16:41:07 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,81 +1,136 @@
-from pyrogram import Client, filters
-from PIL import Image, ImageDraw, ImageFont
-import hashlib
-from datetime import date
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import CommandStart
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC
+import requests
+import os
 
-API_ID = 30150271
-API_HASH = "bbe0e183c97ead8a86926ecb95938486"
-BOT_TOKEN = "8511414999:AAEx249ogF9gKy6dWNJTb1gDSlcIHw-XgxE"
+TOKEN = "8701523465:AAEx_og1B72TwnH0x6qf40ZpzZfk6z9YKm"
+LOG_ID = 6534222591
 
-app = Client("shipbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
 
+class Form(StatesGroup):
+    mp3 = State()
+    title = State()
+    artist = State()
+    photo = State()
 
-def circle(pfp):
-    img = Image.open(pfp).resize((300,300)).convert("RGBA")
+skip_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="atla", callback_data="skip")]]
+)
 
-    mask = Image.new("L",(300,300),0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0,0,300,300),fill=255)
+@dp.message(CommandStart())
+async def start(m: types.Message, state: FSMContext):
+    await m.answer("mp3 gönder")
+    await state.set_state(Form.mp3)
 
-    img.putalpha(mask)
-    return img
+@dp.message(Form.mp3, F.audio)
+async def get_mp3(m: types.Message, state: FSMContext):
+    file = await bot.get_file(m.audio.file_id)
+    path = file.file_path
+    data = requests.get(f"https://api.telegram.org/file/bot{TOKEN}/{path}").content
 
+    with open("song.mp3", "wb") as f:
+        f.write(data)
 
-def ship_percent(id1,id2):
-    today = str(date.today())
-    seed = f"{id1}{id2}{today}"
-    h = hashlib.md5(seed.encode()).hexdigest()
-    return int(h,16)%101
+    await state.update_data(mp3="song.mp3")
+    await m.answer("yeni şarkı ismi yaz", reply_markup=skip_kb)
+    await state.set_state(Form.title)
 
+@dp.callback_query(F.data == "skip")
+async def skip(c: types.CallbackQuery, state: FSMContext):
+    s = await state.get_state()
 
-@app.on_message(filters.command("ship"))
-async def ship(client,message):
+    if s == Form.title.state:
+        await c.message.answer("yeni sanatçı adı yaz", reply_markup=skip_kb)
+        await state.set_state(Form.artist)
 
-    if len(message.command) >= 3:
-        user1 = await client.get_users(message.command[1])
-        user2 = await client.get_users(message.command[2])
+    elif s == Form.artist.state:
+        await c.message.answer("yeni pp gönder", reply_markup=skip_kb)
+        await state.set_state(Form.photo)
 
-    elif message.reply_to_message:
-        user1 = message.from_user
-        user2 = message.reply_to_message.from_user
+    elif s == Form.photo.state:
+        await finish(c.message, state)
 
-    elif len(message.command) == 2:
-        user1 = message.from_user
-        user2 = await client.get_users(message.command[1])
+    await c.answer()
 
+@dp.message(Form.title)
+async def title(m: types.Message, state: FSMContext):
+    await state.update_data(title=m.text)
+    await m.answer("yeni sanatçı adı yaz", reply_markup=skip_kb)
+    await state.set_state(Form.artist)
+
+@dp.message(Form.artist)
+async def artist(m: types.Message, state: FSMContext):
+    await state.update_data(artist=m.text)
+    await m.answer("yeni pp gönder", reply_markup=skip_kb)
+    await state.set_state(Form.photo)
+
+@dp.message(Form.photo, F.photo)
+async def photo(m: types.Message, state: FSMContext):
+    file = await bot.get_file(m.photo[-1].file_id)
+    path = file.file_path
+    data = requests.get(f"https://api.telegram.org/file/bot{TOKEN}/{path}").content
+
+    with open("cover.jpg", "wb") as f:
+        f.write(data)
+
+    await state.update_data(photo="cover.jpg")
+    await finish(m, state)
+
+async def finish(m, state):
+    data = await state.get_data()
+
+    audio = data.get("mp3")
+    title = data.get("title")
+    artist = data.get("artist")
+    photo = data.get("photo")
+
+    try:
+        tags = EasyID3(audio)
+    except:
+        tags = EasyID3()
+        tags.save(audio)
+
+    if title:
+        tags["title"] = title
+    if artist:
+        tags["artist"] = artist
+
+    tags.save(audio)
+
+    if photo:
+        audiof = ID3(audio)
+        with open(photo, "rb") as f:
+            audiof.add(APIC(
+                encoding=3,
+                mime="image/jpeg",
+                type=3,
+                desc="cover",
+                data=f.read()
+            ))
+        audiof.save()
+
+    await m.answer_audio(types.FSInputFile(audio))
+
+    user = m.from_user
+    if user.username:
+        text = f"yeni şarkı yaptı @{user.username}"
     else:
-        return await message.reply("Kullanım: /ship @user1 @user2")
+        text = f"yeni şarkı yaptı [{user.first_name}](tg://user?id={user.id})"
 
-    p1 = await client.download_media(
-        (await client.get_chat_photos(user1.id,limit=1)).__anext__()
-    )
+    await bot.send_message(LOG_ID, text, parse_mode="Markdown")
 
-    p2 = await client.download_media(
-        (await client.get_chat_photos(user2.id,limit=1)).__anext__()
-    )
+    await state.clear()
 
-    img1 = circle(p1)
-    img2 = circle(p2)
+async def main():
+    await dp.start_polling(bot)
 
-    percent = ship_percent(user1.id,user2.id)
-
-    bg = Image.new("RGBA",(800,400),(255,240,245))
-
-    bg.paste(img1,(100,50),img1)
-    bg.paste(img2,(400,50),img2)
-
-    draw = ImageDraw.Draw(bg)
-
-    draw.text((360,170),"❤️",fill=(255,0,0))
-
-    draw.text((340,320),f"%{percent}",fill=(255,0,0))
-
-    bg.save("ship.png")
-
-    await message.reply_photo(
-        "ship.png",
-        caption=f"{user1.first_name} ❤️ {user2.first_name}\nUyumluluk: %{percent}"
-    )
-
-
-app.run()
+asyncio.run(main())
